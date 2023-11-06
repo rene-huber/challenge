7582ce118f0f2d79eb52fb91456dcdcb50c0f251
rene huber <huberskull@gmail.com>
2023-11-06 16:12:49 +0100

    teen or not teen

--- js_conditions_booleads/js-conditions-and-booleans_teenager-check/js/index.js
+++ js_conditions_booleads/js-conditions-and-booleans_teenager-check/js/index.js
@@ -10,4 +10,10 @@ button.addEventListener("click", () => {
   // Exercise:
   // Use conditions and the logical AND operator to write
   // "You are a teen." or "You are not a teen." into the output.
+
+  if (age >= 13 && age <= 19) {
+    output.textContent = "You are a teen.";
+  } else {
+    output.textContent = "You are not a teen.";
+  }
 });
