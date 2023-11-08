a10d2dc3e369adf584c04a4a52c5557ab241328b
rene huber <huberskull@gmail.com>
2023-11-08 11:11:28 +0100

    greetings done

--- js-basics/js-functions-2_greeting-page/js/index.js
+++ js-basics/js-functions-2_greeting-page/js/index.js
@@ -8,26 +8,66 @@ Change the contents of this page depending on the current day and time.
   - 13 - 18: returns "Good Afternoon"
   - 19 - 22: returns "Good Evening"
   - 23 - 5: returns "Good Night"
-
-(HINT: you can get the current time: new Date().getHours() )
-
-- Write a function getDayColor that returns a different color depending on the current weekday:
+  
+  (HINT: you can get the current time: new Date().getHours() )
+  
+  - Write a function getDayColor that returns a different color depending on the current weekday:
   - monday: "darkgray"
   - tuesday - friday: "lightblue"
   - saturday - sunday: "hotpink"
+  
+  (HINT: you can get the current weekday: new Date().getDay() )
+  
+  */
+ 
+ const display = document.querySelector('[data-js="display"]');
+ 
+ 
+ function getGreeting() {
+   // Code here
+   const time= new Date().getHours()
+   console.log(time);
+if(time>=6 && time<=12){
+  return "Good Morning"
 
-(HINT: you can get the current weekday: new Date().getDay() )
+}else if(time>=13 && time<=18){
+  return "Good Afternoon"
 
-*/
+}else if(time>=19 && time<=22){
 
-const display = document.querySelector('[data-js="display"]');
+  return "Good Evening"
 
-function getGreeting() {
-  // Code here
+}else if(time>=23 && time<=5){
+  return "Good Night"
+
+ }
+
 }
+
+
+
 
 function getDayColor() {
   // Code here
+  const day= new Date().getDay()
+  console.log(day);
+if(day==1){
+  return "darkgray"
+
+}else if(day>=2 && day<=5){
+
+  return "lightblue"
+
+}
+
+else if(day>=6 && day<=7){
+  return "hotpink"
+
+ }
+
+ 
+
+    
 }
 
 display.textContent = getGreeting();
