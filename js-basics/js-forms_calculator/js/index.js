d3d5e90498edb1cdcaf5c9740698691728a2ac4d
rene huber <huberskull@gmail.com>
2023-11-10 13:24:06 +0100

    calculator demo

--- js-basics/js-forms_calculator/js/index.js
+++ js-basics/js-forms_calculator/js/index.js
@@ -6,7 +6,7 @@ const resultOutput = document.querySelector('[data-js="result"]');
 const numberA = document.querySelector('[data-js="numberA"]');
 const numberB = document.querySelector('[data-js="numberB"]');
 
-const adition = document.querySelector('[data-js="adition"]');
+const addition = document.querySelector('[data-js="adition"]');
 const subtraction = document.querySelector('[data-js="subtraction"]');
 const multiplication = document.querySelector('[data-js="multiplication"]');
 const division = document.querySelector('[data-js="division"]');
@@ -37,31 +37,54 @@ form.addEventListener("submit", (event) => {
 
   // --v-- write your code here --v--
 
+const checkedOperatorSelected = form.querySelector(':checked');
+
+const operator = checkedOperatorSelected.value;
+console.log(numberA.value, numberB.value, operator);
+
+valueNumberA = Number(numberA.value);
+valueNumberB = Number(numberB.value);
 
 
-  switch (operator.value) {
-    case "add":
-      result = add(numberA, numberB);
-      break;
-    case "subtract":
-      result = subtract(numberA, numberB);
-      break;
-    case "multiply":
-      result = multiply(numberA, numberB);
-      break;
-    case "divide":
-      result = divide(numberA, numberB);
-      break;
-    default:
-      result = "Invalid operator";
-  }
+if (operator === "addition") {
+  result = add(valueNumberA, valueNumberB);
+} else if (operator === "subtraction") {
+  result = subtract(valueNumberA, valueNumberB);
+}
+else if (operator === "multiplication") {
+  result = multiply(valueNumberA, valueNumberB);
+}
+else if (operator === "division") {
+  result = divide(valueNumberA, valueNumberB);
+}
+else {
+  result = "Invalid operator";
+}
 
 
 
   // --^-- write your code here --^--
-
+  
   resultOutput.textContent = result;
 });
 
 
+{/* 
 
+switch (operator.value) {
+  case "addition":
+    result = add(numberA, numberB);
+    break;
+    case "subtract":
+    result = subtract(numberA, numberB);
+    break;
+    case "multiply":
+      result = multiply(numberA, numberB);
+      break;
+      case "divide":
+    result = divide(numberA, numberB);
+    break;
+    default:
+      result = "Invalid operator";
+    }//
+  */} 
