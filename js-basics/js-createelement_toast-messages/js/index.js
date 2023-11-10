dd9828e97a188456e790a2fb7bef446b0b2013bf
rene huber <huberskull@gmail.com>
2023-11-10 15:32:44 +0100

    toast msg add

--- js-basics/js-createelement_toast-messages/js/index.js
+++ js-basics/js-createelement_toast-messages/js/index.js
@@ -6,8 +6,15 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  const newToast = document.createElement("li");
+  newToast.className = "toast-container__message";
+  newToast.textContent = "This is a new toast message";
+  toastContainer.append(newToast);
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = ""; 
 });
+
+
