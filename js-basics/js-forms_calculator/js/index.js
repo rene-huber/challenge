7082aac91c4fbb12fbe3c9b7cca4ca17c3da12ed
rene huber <huberskull@gmail.com>
2023-11-10 12:50:18 +0100

    first round

--- js-basics/js-forms_calculator/js/index.js
+++ js-basics/js-forms_calculator/js/index.js
@@ -3,6 +3,17 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const resultOutput = document.querySelector('[data-js="result"]');
 
+const numberA = document.querySelector('[data-js="numberA"]');
+const numberB = document.querySelector('[data-js="numberB"]');
+
+const adition = document.querySelector('[data-js="adition"]');
+const subtraction = document.querySelector('[data-js="subtraction"]');
+const multiplication = document.querySelector('[data-js="multiplication"]');
+const division = document.querySelector('[data-js="division"]');
+
+const result = document.querySelector('[data-js="result"]');
+
+
 function add(a, b) {
   return a + b;
 }
@@ -26,7 +37,31 @@ form.addEventListener("submit", (event) => {
 
   // --v-- write your code here --v--
 
+
+
+  switch (operator.value) {
+    case "add":
+      result = add(numberA, numberB);
+      break;
+    case "subtract":
+      result = subtract(numberA, numberB);
+      break;
+    case "multiply":
+      result = multiply(numberA, numberB);
+      break;
+    case "divide":
+      result = divide(numberA, numberB);
+      break;
+    default:
+      result = "Invalid operator";
+  }
+
+
+
   // --^-- write your code here --^--
 
   resultOutput.textContent = result;
 });
+
+
+
