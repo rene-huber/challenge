80d277fe8f0ab3ee571d87c109d1fe011d5c653a
rene huber <huberskull@gmail.com>
2023-11-15 10:42:59 +0100

    callback Func login

--- js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
+++ js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
@@ -1,8 +1,24 @@
 console.clear();
 
 function handleUserLogin(onSuccess) {
-  // You might wonder why this function is called `onSuccess`. This will become clear by the fourth challenge! 🤞
+  // You might wonder why this function is called `onSuccess`. 
+  //This will become clear by the fourth challenge! 🤞
   onSuccess();
 }
 
 // Call handleUserLogin below!
+
+function showWelcomeMessage() {
+  console.log("Welcome! You are logged in now.");
+  }
+
+handleUserLogin(showWelcomeMessage);
+
+handleUserLogin(() => {
+  console.log("Welcome! You are logged in now.");
+});
+
+handleUserLogin(function() {
+  console.log("Welcome! You are logged in now.");
+});
+
