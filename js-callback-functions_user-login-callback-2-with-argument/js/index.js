bec880e1d5650c5c34adb79b735ed74b8fe2ed8e
rene huber <huberskull@gmail.com>
2023-11-15 11:11:03 +0100

    files cd js-callback-functions_user-login-callback-3-with-multiple-arguments

--- js-callback-functions_user-login-callback-2-with-argument/js/index.js
+++ js-callback-functions_user-login-callback-2-with-argument/js/index.js
@@ -5,3 +5,34 @@ function handleUserLogin(onSuccess) {
 }
 
 // The exercise starts here!
+
+
+const welcomeUser = userName => console.log(`Welcome Jane Doe! You are logged in now.`);
+
+function welcomeUser(userName) {
+  console.log(`Welcome Jane Doe! You are logged in now.`);
+}
+
+
+welcomeUser();
+
+
+// Método 1: Usando la palabra clave "function"
+function showWelcomeMessage(userName) {
+  console.log(`Welcome ${userName}! You are logged in now.`);
+}
+
+// Método 2: Usando una función anónima
+function handleUserLogin(callback) {
+  // Supongamos que 'Jane Doe' es el usuario para este ejemplo
+  const userName = 'Jane Doe';
+  callback(userName);
+}
+
+// Llamando a handleUserLogin y pasando showWelcomeMessage como el callback (Método 1)
+handleUserLogin(showWelcomeMessage);
+
+// Llamando a handleUserLogin y pasando una función anónima como el callback (Método 2)
+handleUserLogin(function (userName) {
+  console.log(`Welcome ${userName}! You are logged in now.`);
+});
