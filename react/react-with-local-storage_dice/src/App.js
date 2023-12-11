5eb40cb9e35183beba09bc50acbf4e85508ac448
rene-huber <96597641+rene-huber@users.noreply.github.com>
2023-12-11 22:20:07 +0100

    dice - local store

--- react/react-with-local-storage_dice/src/App.js
+++ react/react-with-local-storage_dice/src/App.js
@@ -1,27 +1,26 @@
-// App.js
-import React from 'react';
-import { D6Button } from './components/D6Button';
-import History from './components/History';
-import useLocalStorageState from 'use-local-storage-state';
-import './styles.css';
+import { D6Button } from "./components/D6Button";
+import History from "./components/History";
+import  useLocalStorageState from "use-local-storage-state";
+import "./styles.css";
 
 export default function App() {
-  const [rolls, setRolls] = useLocalStorageState('rolls', []);
-
+  const [rolls, setRolls] = useLocalStorageState("rolls", [])  
+  
   const handleRoll = () => {
-    const newRoll = Math.floor(Math.random() * 6) + 1;
-    setRolls((prevRolls) => (prevRolls ? [...prevRolls, newRoll] : [newRoll]));
+    const newRoll = Math.floor(Math.random() * 6) +1;
+    setRolls((prevRolls) => (prevRolls ? [...prevRolls, newRoll] : [newRoll]))
   };
 
-  const currentRollValue = rolls.length > 0 ? rolls[rolls.length - 1] : null;
+  const value = rolls && rolls.length > 0 ? rolls[rolls.length - 1] : null;
+
 
   return (
     <div className="app">
       <main className="app__main">
-        <D6Button value={currentRollValue} onRoll={handleRoll} />
+        <D6Button value={value} onRoll={handleRoll}/>
       </main>
       <aside className="app__aside">
-        <History rolls={rolls || []} />
+        <History rolls={rolls || [] } />
       </aside>
     </div>
   );
