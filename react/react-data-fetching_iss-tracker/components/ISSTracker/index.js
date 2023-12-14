6ec72ec4a193bb6f41b1e8eb1da5e13c83c3a664
rene-huber <96597641+rene-huber@users.noreply.github.com>
2023-12-14 12:13:21 +0100

    done

--- react/react-data-fetching_iss-tracker/components/ISSTracker/index.js
+++ react/react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -8,11 +8,9 @@ const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 const fetcher = async url => {
   const res = await fetch(url)
  
-  // If the status code is not in the range 200-299,
-  // we still try to parse and throw it.
   if (!res.ok) {
     const error = new Error('An error occurred while fetching the data.')
-    // Attach extra info to the error object.
+   
     error.info = await res.json()
     error.status = res.status
     throw error
@@ -41,7 +39,7 @@ export default function ISSTracker() {
 console.log(data, coords)
 
  const handleReload = () => {
-    mutate(); // Llama a mutate para refrescar los datos
+    mutate(); 
   };
 
   // async function getISSCoords() {
